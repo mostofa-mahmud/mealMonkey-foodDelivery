e2baa633dfef6c01e40ee46026681571c000e62b
mostofa-mahmud <mostofamahmud23@gmail.com>
2021-11-13 00:06:40 +0600

    5rth

--- lib/src/pages/entry/intro_page.dart
+++ lib/src/pages/entry/intro_page.dart
@@ -9,66 +9,85 @@ class Intro_page extends StatefulWidget {
 
 class _Intro_pageState extends State<Intro_page> {
 
+
+  late final PageController _controller;
   final List<IntroModel> _data = IntroModel.data;
+  int _activeIndex =0;
 
   @override
+  void initState(){
+    super.initState();
+    _controller = PageController();
+    _controller.addListener(() {
+      setState(() {
+        _activeIndex = _controller.page!.round();
+
+      });
+    });
+  }
+
+
   Widget build(BuildContext context) {
     return Scaffold(
       
-      body: Column(
+      body: Stack(
+        alignment: Alignment.bottomCenter,
         children: [
-          Expanded(
-            child: PageView.builder(
-              itemCount: _data.length,
-                itemBuilder: (context, index){
-                  return Column(
-                    children: [
-                      SizedBox(height: 150.h,),
-                      Image.asset(
-                        _data[index].imagePath!,
-                        width: 700.w,
-                      ),
-                    ],
-                  );
-                  },
-            ),
+          PageView.builder(
+            controller: _controller,
+            itemCount: _data.length,
+              itemBuilder: (context, index){
+                return Column(
+                  children: [
+                    SizedBox(height: 150.h,),
+                    Image.asset(
+                      _data[index].imagePath!,
+                      height: 1000.h,
+                    ),
+                  ],
+                );
+                },
           ),
           
-          SliderIndicator(
-              length: _data.length,
-              activeIndex: 0,
-              indicator: Icon(
-                  Icons.fiber_manual_record_rounded,
-                color: placeholderColor,
-                size: 32.w,
-              ),
-              activeIndicator: Icon(
-                  Icons.fiber_manual_record_rounded,
-                color: mainColor,
-                size: 32.w,
-              ),
-          ),
-          SizedBox(
-              height: 150.h,
-              child: Text(
-                  _data[0].title!,
-                style: Theme.of(context).textTheme.headline4!.copyWith(
-                    color: secondaryFontColor,
-                    fontWeight: FontWeight.bold,
-                    height: 4.h
+          Padding(
+            padding: EdgeInsets.only(bottom: 900.h),
+            child: SliderIndicator(
+                length: _data.length,
+                activeIndex: _activeIndex,
+                indicator: Icon(
+                    Icons.fiber_manual_record_rounded,
+                  color: placeholderColor,
+                  size: 32.w,
                 ),
-              )
+                activeIndicator: Icon(
+                    Icons.fiber_manual_record_rounded,
+                  color: mainColor,
+                  size: 32.w,
+                ),
+            ),
           ),
-          SizedBox(
-              height: 150.h,
-              child: Text(
-                  _data[0].description!,
-                style: Theme.of(context).textTheme.caption!.copyWith(
-                  color: secondaryFontColor,
+          Padding(
+            padding: EdgeInsets.only(bottom: 700.h),
+            child: Text(
+                _data[_activeIndex].title!,
+              style: Theme.of(context).textTheme.headline4!.copyWith(
+                  color: primaryFontColor,
                   fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 400.h),
+            child: SizedBox(
+              height: 200.h,
+              child: Text(
+                  _data[_activeIndex].description!,
+                style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                  color: secondaryFontColor,
                   height: 4.h
                 ),
               ),
+            ),
           ),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 60.w, vertical: 120.h),
@@ -77,17 +96,21 @@ class _Intro_pageState extends State<Intro_page> {
                 Expanded(
                   child: ElevatedButton(
                       onPressed: (){
-
+                        if((_activeIndex + 1)>= _data.length) return;
+                        _controller.animateToPage(
+                            _activeIndex + 1,
+                            duration: const Duration(milliseconds: 250),
+                            curve: Curves.easeIn
+                        );
                       },
-                      style: ButtonStyle(
-                        shape: MaterialStateProperty.all<OutlinedBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(40.r)
-                          )
+                      style: ElevatedButton.styleFrom(
+                        primary: mainColor,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40),
                         ),
-                        backgroundColor: MaterialStateProperty.all<Color>(mainColor),
-                        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.all(15))
+                        padding: EdgeInsets.all(15),
                       ),
+
                       child: Text('Next')
                   ),
                 ),
