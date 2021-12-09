d15731661d8cb722480c2b2192bd60e25c64e8d8
mostofa-mahmud <mostofamahmud23@gmail.com>
2021-12-09 20:59:26 +0600

    16th

--- lib/src/pages/entry/login_page.dart
+++ lib/src/pages/entry/login_page.dart
@@ -1,120 +1,127 @@
 part of pages;
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  GlobalKey<FormState> _formState = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 60.w),
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            children: [
+        child: Form(
+          key: _formState,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              children: [
 
-              SizedBox(height: 120.h,),
-              Text(
-                "Login",
-                style: Theme.of(context).textTheme.headline4!.copyWith(
-                  color: primaryFontColor,
+                LoginRegisterHeader(title: 'Login', subTitle: 'Add details to Log In'),
+
+                TextFormField(
+                  style: TextStyle(color: primaryFontColor),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (val){
+                    if(val== null || val.isEmpty)
+                      return 'Please enter your email';
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Your Email',
+                    isCollapsed: true,
+                  ),
                 ),
-              ),
 
-              SizedBox(height: 40.h,),
+                SizedBox(height: verticalPadding,),
 
-              Text("Add your details to Login",
-                style: Theme.of(context).textTheme.subtitle1!.copyWith(
-                  color: secondaryFontColor,
+                TextFormField(
+                  style: TextStyle(color: primaryFontColor),
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    isCollapsed: true,
+                  ),
                 ),
-              ),
-
-              SizedBox(height: 120.h,),
 
 
-              CustomTextFormField(
-                labelText: 'Your Email',
-              ),
-
-              SizedBox(height: 60.h,),
-
-
-              CustomTextFormField(
-                labelText: 'Password',
-              ),
-
-              SizedBox(height: 60.h,),
+                SizedBox(height: verticalPadding,),
 
 
 
-              ElevatedButton(
-                  onPressed: (){},
-                  child: Text("Login")
-              ),
-
-
-              SizedBox(height: 60.h,),
-
-
-              Text('Forgot your password?',
-                style: Theme.of(context).textTheme.subtitle1!.copyWith(
-                  color: secondaryFontColor,
+                ElevatedButton(
+                    onPressed: (){},
+                    child: Text("Login")
                 ),
-              ),
-              Text('Or Login with',
-                style: Theme.of(context).textTheme.subtitle1!.copyWith(
-                  color: secondaryFontColor,
+
+
+                SizedBox(height: verticalPadding,),
+
+
+                GestureDetector(
+                  onTap: (){
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => EnterEmailPage()));
+                  },
+                  child: Text('Forgot your password?',
+                    style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                      color: secondaryFontColor,
+                    ),
+                  ),
                 ),
-              ),
 
 
-              SizedBox(height: 60.h,),
-
-              ElevatedButton.icon(
-                onPressed: (){
-
-                },
-                icon: Icon(FontAwesomeIcons.facebookF),
-                label: Text("Login with facebook"),
-                style: ElevatedButton.styleFrom(
-                  primary: Color(0xff3b5998),
+                Text('Or Login with',
+                  style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                    color: secondaryFontColor,
+                  ),
                 ),
-              ),
 
 
-              SizedBox(height: 40.h,),
+                SizedBox(height: verticalPadding,),
 
-              ElevatedButton.icon(
-                onPressed: (){
+                ElevatedButton.icon(
+                  onPressed: (){
 
-                },
-                icon: Icon(FontAwesomeIcons.googlePlusG),
-                label: Text("Login with Google"),
-                style: ElevatedButton.styleFrom(
-                  primary: Color(0xffdb4a39),
+                  },
+                  icon: Icon(FontAwesomeIcons.facebookF),
+                  label: Text("Login with facebook"),
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xff3b5998),
+                  ),
                 ),
-              ),
 
 
-              Expanded(child: SizedBox.shrink()),
-              Text.rich(
-                  TextSpan(
-                      text: 'Don\'t have an account?',
-                      style: Theme.of(context).textTheme.bodyText2,
-                      children: [
-                        TextSpan(text: ' Sign up',
-                            style: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                              fontWeight: FontWeight.bold,
-                            )
-                        ),
-                      ]
-                  )
-              ),
-              SizedBox(height: 50.h,),
+                SizedBox(height: 40.h,),
+
+                ElevatedButton.icon(
+                  onPressed: (){
+
+                  },
+                  icon: Icon(FontAwesomeIcons.googlePlusG),
+                  label: Text("Login with Google"),
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xffdb4a39),
+                  ),
+                ),
 
 
-            ],
+                Expanded(child: SizedBox.shrink()),
+                LoginRegisterFooter(
+                    question: 'Don\'t have an account? ',
+                    actionText:' Sign up',
+                    action: (){
+                      Navigator.pushReplacementNamed(context, kRouteRegister);
+                    }),
+                SizedBox(height: 50.h,),
+
+
+
+              ],
+            ),
           ),
         ),
       ),
